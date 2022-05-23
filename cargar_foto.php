4bf8f8dbec4d1763e04ba5ed369ed996695b12a6
Christiam De la Cruz <christiamdelacruz@hotmail.com>
2022-05-23 00:00:19 -0500

    redireccion al cargar la foto

--- cargar_foto.php
+++ cargar_foto.php
@@ -19,10 +19,10 @@ if($_FILES["archivo"]){
         $result = mysqli_query($conexion, $sql);
         if($result){
             echo "<script>alert('Se registro foto');</script>";
-            header('Location:' . getenv('HTTP_REFERER'));
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
         } else{
             print("Errormessage: %s\n". mysqli_error($conexion));
-            header('Location:' . getenv('HTTP_REFERER'));
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
         }
     
     }
