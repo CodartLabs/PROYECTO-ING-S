60032358fd7547015b9e1981263dabc2a2928fbc
juniorcv21 <juniorfox1998@gmail.com>
2022-05-23 13:52:01 -0500

    try catch

--- cargar_foto.php
+++ cargar_foto.php
@@ -8,28 +8,35 @@ $conexion  = new mysqli($host, $user, $pass, $database);
 /* $id = $_SESSION['id_usuario']; */
 $id = $_POST["valorid"]; 
 
-if($_FILES["archivo"]){
-    $nombre_base = basename($_FILES["archivo"]["name"]);
-    $nombre_final = date("d-m-y")."-".date("H-i-s")."-".$nombre_base;
-    $ruta = "assets/users/".$nombre_final;
-    $subirarchivo = move_uploaded_file($_FILES["archivo"]["tmp_name"],$ruta);
-
-    if($subirarchivo){
-        $sql = "UPDATE usuarios SET foto = '$nombre_final' WHERE id_usuario = $id";
-        $result = mysqli_query($conexion, $sql);
-        if($result){
-            echo "<script>alert('Se registro foto');</script>";
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-        } else{
-            print("Errormessage: %s\n". mysqli_error($conexion));
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
+try {
+    if($_FILES["archivo"]){
+        $nombre_base = basename($_FILES["archivo"]["name"]);
+        $nombre_final = date("d-m-y")."-".date("H-i-s")."-".$nombre_base;
+        $ruta = "assets/users/".$nombre_final;
+        $subirarchivo = move_uploaded_file($_FILES["archivo"]["tmp_name"],$ruta);
+    
+        if($subirarchivo){
+            $sql = "UPDATE usuarios SET foto = '$nombre_final' WHERE id_usuario = $id";
+            $result = mysqli_query($conexion, $sql);
+            if($result){
+                echo "<script>alert('Se registro foto');</script>";
+                header('Location: ' . $_SERVER['HTTP_REFERER']);
+            } else{
+                print("Errormessage: %s\n". mysqli_error($conexion));
+                header('Location: ' . $_SERVER['HTTP_REFERER']);
+            }
+        
         }
     
+    }else{
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
-
-}else{
-    echo "Error al subir archivo";
-    header('Location:' . getenv('HTTP_REFERER'));
+} catch (Throwable $th) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+} finally{
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
+
+
 
 ?>
