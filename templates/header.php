c60c309595f72fed3de283b1f8ce651d38e99b41
Christiam De la Cruz <christiamdelacruz@hotmail.com>
2022-05-23 00:38:27 -0500

    pequeñas mejoras

--- templates/header.php
+++ templates/header.php
@@ -52,10 +52,6 @@
         </li>
     <?php } ?>
     </ul>
-    <form class="form-inline my-2 my-lg-0" method="GET">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" name="busqueda" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
     
   </div>
 </nav>
